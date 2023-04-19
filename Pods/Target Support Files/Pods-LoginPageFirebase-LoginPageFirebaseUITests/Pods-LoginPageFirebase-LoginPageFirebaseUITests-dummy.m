c358ec85e480e7813022adb00e28aec2c1f823a9
dominhgiangbboy <giangdm@smartosc.com>
2023-04-19 17:07:52 +0700

    Ignore Pods

--- Pods/Target Support Files/Pods-LoginPageFirebase-LoginPageFirebaseUITests/Pods-LoginPageFirebase-LoginPageFirebaseUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoginPageFirebase_LoginPageFirebaseUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LoginPageFirebase_LoginPageFirebaseUITests
-@end
